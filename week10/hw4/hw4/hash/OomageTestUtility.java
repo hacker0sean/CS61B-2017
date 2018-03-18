60ab21b713828e3150283a51953908f180f450ad
sean <997647204@qq.com>
2018-03-18 13:59:02 +0800

    hw04

---
+++ week10/hw4/hw4/hash/OomageTestUtility.java
@@ -0,0 +1,30 @@
+package hw4.hash;
+
+import java.util.List;
+
+public class OomageTestUtility {
+    public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
+        /* TODO: Write a utility function that returns true if the given oomages 
+         * have hashCodes that would distribute them fairly evenly across
+         * M buckets. To do this, convert each oomage's hashcode in the
+         * same way as in the visualizer, i.e. (& 0x7FFFFFF) % M.
+         * and ensure that no bucket has fewer than N / 50
+         * Oomages and no bucket has more than N / 2.5 Oomages.
+         */
+        int size = oomages.size();
+        int bucketNum;
+        int[] buckets = new int[M];
+        for (Oomage i : oomages){
+            bucketNum = (i.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum]++;
+        }
+        for (int i : buckets){
+            double higher = size / 2.5;
+            double lower = size / 50 * 1.0;
+            if(i < lower || i > higher){
+                return false;
+            }
+        }
+        return true;
+    }
+}
